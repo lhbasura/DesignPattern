e7c17d66c77252911a6e3be2014639be2b6f9fa1
LiuHongbo <hongbo.liu@geneegroup.com>
2019-04-24 13:36:49 +0800

    单件模式"

---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,13 @@
+package Singleton;
+
+public class Singleton {
+    private static Singleton uniqueInstance=new Singleton();
+    private Singleton()
+    {
+    }
+    public static Singleton getInstance()
+    {
+        return uniqueInstance;
+    }
+
+}
